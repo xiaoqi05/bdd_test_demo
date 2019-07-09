797b946804c863226b4fd49b8fed33fbe7b0c46f
qxiao <qxiao@thoughtworks.com>
2019-07-09 14:17:37 +0800

    add test

--- src/test/java/com/demo/string/LocaleUtilsTest.java
+++ src/test/java/com/demo/string/LocaleUtilsTest.java
@@ -9,6 +9,13 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class LocaleUtilsTest {
 
+    private static final Locale LOCALE_EN = new Locale("EN", "");
+    private static final Locale LOCALE_EN_US = new Locale("en", "US");
+    private static final Locale LOCALE_EN_US_ZZZZ = new Locale("en", "US", "ZZZZ");
+    private static final Locale LOCALE_FR = new Locale("fr", "");
+    private static final Locale LOCALE_FR_CA = new Locale("fr", "CA");
+    private static final Locale LOCALE_QQ = new Locale("qq", "");
+    private static final Locale LOCALE_QQ_ZZ = new Locale("qq", "ZZ");
     private LocaleUtils localeUtils;
 
     @Before
@@ -64,6 +71,24 @@ public class LocaleUtilsTest {
     }
 
     @Test (expected = IllegalArgumentException.class)
+    public void should_throw_exception_when_input_str_length_equal_4_and_start_with_underlined() {
+        //given
+        String fourLengthStr = "_123";
+        //when
+        localeUtils.toLocale(fourLengthStr);
+        //then
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void should_throw_exception_for_special_str_length_above_4_and_start_with_underlined() {
+        //given
+        String fourLengthStr = "_AAaa";
+        //when
+        localeUtils.toLocale(fourLengthStr);
+        //then
+    }
+
+    @Test (expected = IllegalArgumentException.class)
     public void should_throw_exception_when_lower_case_in_2_index_and_start_with_underlined() {
         //given
         String inputStr = "_aA";
@@ -80,4 +105,16 @@ public class LocaleUtilsTest {
         localeUtils.toLocale(inputStr);
         //then
     }
+
+    @Test
+    public void should_get_locale_for_alpha_3_language_code() {
+        //given
+        String inputStr = "_EN";
+        Locale localeEn = new Locale("", "EN");
+        //when
+        Locale locale = localeUtils.toLocale(inputStr);
+        //then
+        assertThat(locale).isEqualTo(localeEn);
+    }
+
 }
